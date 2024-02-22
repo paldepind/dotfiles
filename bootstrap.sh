c43d72663b8bc4013e274b88b61fb81d9c26412b
Simon Friis Vindum <simonfv@gmail.com>
2024-02-22 07:55:20 +0100

    Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,30 @@
+# Generate a new SSH key and add it to GitHub
+# Clone the dotfiles
+
+if ! command -v git &> /dev/null; then
+	sudo dnf install -q git
+fi
+
+if ! command -v gh &> /dev/null; then
+	sudo dnf install -q gh
+fi
+
+if [ ! -e ~/.ssh/id_ed25519.pub ]; then
+	echo "Generating SSH key"
+	exit
+	ssh-keygen -t ed25519 -C "simonfv@gmail.com"
+	eval "$(ssh-agent -s)"
+	ssh-add ~/.ssh/id_ed25519
+
+	echo "Login to GitHub"
+	gh auth login
+
+	# gh auth refresh -h github.com -s admin:ssh_signing_key
+
+	echo "Add SSH key to GitHub"
+	gh ssh-key add ~/.ssh/id_ed25519.pub --type signing
+fi
+
+if [ ! -e ~/config ]; then
+	gh repo clone paldepind/dotfiles ~/config
+fi
