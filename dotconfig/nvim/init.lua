c6180d59d59b1cc8fece40c90a31e78011ca1198
Simon Friis Vindum <simonfv@gmail.com>
2024-03-04 15:54:32 +0100

    Add TS setup for Neovim

--- dotconfig/nvim/init.lua
+++ dotconfig/nvim/init.lua
@@ -238,6 +238,41 @@ require("lazy").setup({
     end
   },
 
+  -- TypeScript & JavaScript
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+    config = function()
+      local tt = require("typescript-tools")
+      tt.setup {
+        on_attach = function()
+          -- -- Enable completion triggered by <c-x><c-o>
+          -- vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+          -- Mappings.
+          local bufopts = { noremap=true, silent=true, buffer=bufnr }
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+          vim.keymap.set('n', '<M-K>', vim.lsp.buf.signature_help, bufopts)
+          -- vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+          -- vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+          -- vim.keymap.set('n', '<leader>wl', function()
+          --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          -- end, bufopts)
+          -- vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
+          --
+          vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, bufopts)
+          vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+        end
+        -- on_attach = on_attach,
+      }
+    end
+  },
+
   -- Coq
   -- filetype plugin indent on
   {
